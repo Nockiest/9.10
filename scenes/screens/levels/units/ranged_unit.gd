d29a750a56b49b51eb390ab1c236ecebc60c12d0
Andrew <ondralukes06@seznam.cz>
2023-10-12 19:42:40 +0200

    more images to the unit bar

--- scenes/screens/levels/units/ranged_unit.gd
+++ scenes/screens/levels/units/ranged_unit.gd
@@ -25,7 +25,7 @@ func _ready():
 func update_stats_bar():
 	super.update_stats_bar()
 	if action_component:
-		%Ammo2.text = "Ammo "+str(action_component.ammo)
+		%Ammo2.text =  str(action_component.ammo)
 #	$UnitStatsBar/VBoxContainer/Attacks.text = "Attacks "+str(action_component.remain_actions)
 
 func _on_ranged_attack_comp_ammo_changed(_ammo):
